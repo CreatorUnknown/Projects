f01720e3b73509a75e92d09366b494a8b34a64d2
CreatorUnknown <74527366+CreatorUnknown@users.noreply.github.com>
2021-10-09 11:17:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function mn(){
+    window.open("https://creatorunknown.github.io/MobileNet/")
+}
+
+function ma(){
+    window.open("https://x.thunkable.com/web-build/index.html?projectId=61611d661fb6f50010627202&noframe=1&responsive=1&screenId=undefined")
+}
